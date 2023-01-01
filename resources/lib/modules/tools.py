a2348d1e6cf25ea82e1aa1453e77aaf67f79e45f
peno64 <32430988+peno64@users.noreply.github.com>
2023-01-01 20:16:03 +0100

    fix backup for non unicode files

--- resources/lib/modules/tools.py
+++ resources/lib/modules/tools.py
@@ -150,12 +150,22 @@ def FIX_SPECIAL():
                      dp.update(0,"Fixing", "[COLOR dodgerblue]" + file + "[/COLOR]")
                  else:
                      dp.update(0,"Fixing" + '\n' + "[COLOR dodgerblue]" + file + "[/COLOR]")
-                 a=open((os.path.join(root, file))).read()
-                 b=a.replace(HOME, 'special://home/')
-                 f= open((os.path.join(root, file)), mode='w')
-                 f.write(str(b))
-                 f.close()
 
+                 try:
+                     a = open((os.path.join(root, file)), 'r', encoding='utf-8').read()
+                     b = a.replace(HOME, 'special://home/')
+                     f = open((os.path.join(root, file)), mode='w', encoding='utf-8')
+                     f.write(unicode(b))
+                     f.close()
+                 except:
+                     try:
+                         a = open((os.path.join(root, file)), 'r').read()
+                         b = a.replace(HOME, 'special://home/')
+                         f = open((os.path.join(root, file)), mode='w')
+                         f.write(unicode(b))
+                         f.close()
+                     except:
+                         pass
 
 def skinswap():
 
